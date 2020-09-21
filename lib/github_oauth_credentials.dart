9d1e51d5f1ce34db7e49de7a81da4ae05c342ed3
Thomas Erhel <thomas.erhel@gmail.com>
2020-09-21 05:05:22 +0200

    Add authentication

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '6b7f60cff08f936d608a';
+const githubClientSecret = 'd0ed8522188a63926eea2c8eb44bef704db85b3f';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
